1d2c7d78a5cac6dc8915b4d72176e86ef2601a03
Big Dev <badejoemmanuel805@gmail.com>
2025-10-24 08:50:59 +1300

    Add google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,30 +1,30 @@
-import { PropsWithChildren } from "react"
-import "./globals.css"
-import { Metadata } from "next"
-import { Poppins } from "next/font/google"
-import AuthServer from "@/context/AuthServer"
-import { Toaster } from "@/components/ui/sonner"
-
-
+import { PropsWithChildren } from "react";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import "./globals.css";
+import { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import AuthServer from "@/context/AuthServer";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
-  title: "Free screen recorder for Mac and PC | LooP"
-}
+    title: "Free screen recorder for Mac and PC | LooP",
+};
 
 // const poppins = Poppins({ weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"], variable: "--poppins" })
 
-
 export default function RootLayout({ children }: PropsWithChildren) {
-  return (
-    <>
-      <html lang="en">
-        <body className={""} cz-shortcut-listen="true">
-          <AuthServer>
-            <Toaster />
-            {children}
-          </AuthServer>
-        </body>
-      </html>
-    </>
-  )
+    const id = process.env.MEASUREMENT_ID || "G-XXXXXXX";
+    return (
+        <>
+            <html lang="en">
+                <body className={""} cz-shortcut-listen="true">
+                    <AuthServer>
+                        <Toaster />
+                        {children}
+                    </AuthServer>
+                </body>
+                <GoogleAnalytics gaId={id} />
+            </html>
+        </>
+    );
 }
